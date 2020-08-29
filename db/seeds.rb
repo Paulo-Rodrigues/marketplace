cd9a5341393016745079f9a785978a5dfec97396
Paulo Rodrigues <paulor2222@gmail.com>
2020-08-29 18:25:31 -0300

    initial seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,55 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+p 'seeding'
+
+User.create!(
+  name: Faker::Name.first_name,
+  surname: Faker::Name.last_name,
+  department: Faker::Lorem.word,
+  email: 'test@company.com',
+  password: 'password'
+)
+
+3.times do |x|
+  User.create!(
+    name: Faker::Name.first_name,
+    surname: Faker::Name.last_name,
+    department: Faker::Lorem.word,
+    email: "test#{x}@company.com",
+    password: 'password'
+  )
+  p User.last.name
+end
+
+2.times do |x|
+  User.create!(
+    name: Faker::Name.first_name,
+    surname: Faker::Name.last_name,
+    department: Faker::Lorem.word,
+    email: "test#{x}@othercompany.com",
+    password: 'password'
+  )
+  p User.last.name
+end
+
+20.times do |x|
+  Product.create!(
+    name: "Product#{x}",
+    description: "Some description",
+    user: User.where(id: [1..4]).sample , #ids from users from company 1
+    company: User.first.company
+  )
+  Product.last.image.attach(io: File.open('./spec/fixtures/files/image.jpg'), filename: 'image.jpg')
+  p Product.last.name
+end
+
+10.times do |x|
+  Product.create!(
+    name: "Product#{x}",
+    description: "Some description",
+    user: User.where(id: [5..6]).sample , #ids from users from company 2
+    company: User.last.company
+  )
+  Product.last.image.attach(io: File.open('./spec/fixtures/files/image.jpg'), filename: 'image.jpg')
+  p Product.last.name
+end
+
+p 'done'
