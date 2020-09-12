a30ca4234e516b7a1a87e48895b056f993c5e70f
Paulo Rodrigues <paulor2222@gmail.com>
2020-09-11 23:32:59 -0300

    admin reports(WIP)

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,5 +1,5 @@
 class Report < ApplicationRecord
   belongs_to :user
-  belongs_to :reportable, polymorphic: true
+s belongs_to :reportable, polymorphic: true
 
 end
--- spec/features/admin/admin_receive_report_spec.rb
+++ spec/features/admin/admin_receive_report_spec.rb
@@ -9,14 +9,17 @@ feature 'Admin receive a report on user' do
     expect(page).to have_link('Denúncias')
   end
 
-  xscenario 'receive a report' do
+  scenario 'receive a report' do
     user1 = create(:user)
-    user2 = create(:user, email: 'other@company.com')
-    Report.create!(reportable: user2, user: user1, reason: 'good reason')
+    user2 = create(:user_with_profile, email: 'other@company.com')
+    report = Report.create!(reportable: user2, user: user1, reason: 'good reason')
     admin = create(:admin); login_as(admin)
 
     visit admin_root_path
 
-    expect(page).to have_link('Denúncias')
+    click_link 'Denúncias'
+
+    expect(current_path).to eq(admin_reports_path)
+    expect(page).to have_content(user2.name)
   end
 end
